83c1ca8b2abe31124bb408a61856aff829933a2d
Yismen Jorge <yismen.jorge@gmail.com>
2015-12-08 14:15:07 -0400

    csrf

--- public/assets/plugins/startrap-agency/js/contact_me.js
+++ public/assets/plugins/startrap-agency/js/contact_me.js
@@ -1,9 +1,9 @@
 $(function() {
-    $.ajaxSetup({
-        headers: {
-            'X-CSRF-TOKEN': $('meta[name="csrf_token"]').attr('content')
-        }
-    });
+    // $.ajaxSetup({
+    //     headers: {
+    //         'X-CSRF-TOKEN': $('meta[name="csrf_token"]').attr('content')
+    //     }
+    // });
 
     $("#contactForm input,#contactForm textarea").jqBootstrapValidation({
         preventSubmit: true,
