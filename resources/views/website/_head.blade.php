7cf64adc5fdcbb364e71521d2ca8e4ec12c3ef5f
Yismen Jorge <yismen.jorge@gmail.com>
2015-12-25 12:31:50 -0400

    Updated home page header text

--- resources/views/website/_head.blade.php
+++ resources/views/website/_head.blade.php
@@ -5,7 +5,7 @@
 				<div class="container">
 					<div class="intro-text">
 						<div class="intro-lead-in">We deliver quality!</div>
-						<div class="intro-heading">Best Raw Hides and Web Blue Hides</div>
+						<div class="intro-heading">Best Raw Hides and Wet Blue Hides</div>
 						<a href="#services" class="page-scroll btn btn-xl">Tell Me More</a>
 					</div>
 				</div>
