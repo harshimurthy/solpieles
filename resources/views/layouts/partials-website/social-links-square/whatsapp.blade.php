f107079e9d0aff749cefd673ee0adae97c574b18
Yismen Jorge <yismen.jorge@gmail.com>
2016-02-13 12:21:42 -0400

    Updated whatsapp links

--- resources/views/layouts/partials-website/social-links-square/whatsapp.blade.php
+++ resources/views/layouts/partials-website/social-links-square/whatsapp.blade.php
@@ -1,4 +1,4 @@
 
 
 
-        <a href="whatsapp://send?abid=username&text=HeyThere!" title="Whatsapp" target="_blank"><i class="fa fa-whatsapp fa-2x"></i></a>
+        <a href="whatsapp://send?abid=+18295213304&text=HeyThere!" title="Whatsapp" target="_blank"><i class="fa fa-whatsapp fa-2x"></i></a>
--- resources/views/layouts/partials-website/social-links/whatsapp.blade.php
+++ resources/views/layouts/partials-website/social-links/whatsapp.blade.php
@@ -1,2 +1,2 @@
 
-        <a href="whatsapp://send?abid=username&text=HeyThere!" title="Whatsapp" target="_blank"><i class="fa fa-whatsapp fa-2x"></i></a>
+        <a href="whatsapp://send?abid=+18295213304&text=HeyThere!" title="Whatsapp" target="_blank"><i class="fa fa-whatsapp fa-2x"></i></a>
