fe5ec3f8a041dfdd2129a698223a6baf5783ac13
Yismen Jorge <yismen.jorge@gmail.com>
2016-02-21 14:06:15 -0400

    Test

--- resources/views/images/edit.blade.php
+++ resources/views/images/edit.blade.php
@@ -23,12 +23,30 @@
 	
 							<div class="col-sm-4">
 								<div class="form-group">
+										
 									<h4>Image Preview: </h4>
 									<img src="{{ asset($image->path) }}" class="img-responsive" alt="Image">
 								</div>
 							</div>
+							
 	
 						{!! Form::close() !!}
+
+						<div class="col-sm-4">
+								
+								<div class="form-group">
+									<form action="{{ route('admin.images.destroy', $image->id) }}" method="POST" class="" style="">
+									    {!! csrf_field() !!}
+									    {!! method_field('DELETE') !!}
+									
+									    <button type="submit" id="delete-image-{{ $image->id }}" class="btn btn-danger form-control" style="" name="deleteBtn">
+									        
+									        	<i class="fa fa-btn fa-remove"></i> Delete Image
+									       
+									    </button>
+									</form>
+								</div>
+							</div>
 					</div>
 
 						
