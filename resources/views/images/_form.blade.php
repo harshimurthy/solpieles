ec99012ab407dbce50b36101c2ef22f87e143dbd
Yismen Jorge <yismen.jorge@gmail.com>
2016-02-22 14:10:47 -0400

    Updated fontawesome icons

--- resources/views/images/_form.blade.php
+++ resources/views/images/_form.blade.php
@@ -3,7 +3,7 @@
 <div class="form-group {{ $errors->has('name') ? 'has-error' : null }}">
 	{!! Form::label('name', 'Image Name:', ['class'=>'']) !!}
 	<div class="input-group">
-		<div class="input-group-addon"><i class="fa fa-search"></i></div>
+		<div class="input-group-addon"><i class="fa fa-font"></i></div>
 		{!! Form::input('text', 'name', null, ['class'=>'form-control', 'placeholder'=>'Image Name']) !!}
 	</div>
 </div>
@@ -12,7 +12,7 @@
 <div class="form-group {{ $errors->has('tags') ? 'has-error' : null }}">
 	{!! Form::label('tags', 'Images Tags:', ['class'=>'']) !!}
 	<div class="input-group">
-		<div class="input-group-addon"><i class="fa fa-search"></i></div>
+		<div class="input-group-addon"><i class="fa fa-font"></i></div>
 		{!! Form::input('text', 'tags', null, ['class'=>'form-control', 'placeholder'=>'Images Tags']) !!}
 	</div>
 		<span class="help-text">Separate multiple tags by comma.</span>
@@ -22,7 +22,7 @@
 <div class="form-group {{ $errors->has('image') ? 'has-error' : null }}">
 	{!! Form::label('image', 'Images to Upload:', ['class'=>'']) !!}
 	<div class="input-group">
-		<div class="input-group-addon"><i class="fa fa-search"></i></div>
+		<div class="input-group-addon"><i class="fa fa-file"></i></div>
 		{!! Form::file('image', [ 'class'=>'form-control', 'placeholder'=>'Images Tags']) !!}
 	</div>
 </div>
